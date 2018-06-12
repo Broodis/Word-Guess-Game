56d7b5af535594ca069d3340d09bc4e02326c6eb
Broodis <j.porter712@gmail.com>
2018-06-11 19:36:30 -0700

    Second Commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+var words = ['ethereum', 'bitcoin', 'neo', 'ripple', 'litecoin', 'stellar', 'cardano', 'monero', 'vechain', 'dash', 'omisego', 'decred', 'siacoin', 'augur', 'golem', 'wanchain', 'stratis', 'komodo', 'ark', 'ardor', 'pascal']
+
+function chooseWord (){
+    words[Math.floor(Math.random() * words.length)];
+}
